a1b8d4f109cea45828b4e3df27024917d9be451d
natajopia <natajopia25@outlook.com>
2022-12-20 18:50:31 -0300

    limite 16 caracteres

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import validator from './validator.js'
 const nombre = prompt("Para una mejor comunicación ingresa tu nombre");
 
 //funcion para que solo admita numeros
-let inputEspacioNumeros= document.getElementById("inputEspacioNumeros");
+let inputEspacioNumeros = document.getElementById("inputEspacioNumeros");
 inputEspacioNumeros.addEventListener("keypress", (event) => {
-event.preventDefault()
-let codigoKey= event.keyCode
-let valorKey= String.fromCharCode (codigoKey);
- console.log ("valorKey");
-let valor=parseInt (valorKey);
- console.log (valor)
- if (valor){
-inputEspacioNumeros.value += valor
- }
+  event.preventDefault()
+  let codigoKey = event.keyCode
+  let valorKey = String.fromCharCode(codigoKey);
+  console.log("valorKey");
+  let valor = parseInt(valorKey);
+  console.log(valor)
+  if (valor && inputEspacioNumeros.value.length <16) {
+    inputEspacioNumeros.value += valor
+  }
 })
 
 
@@ -27,7 +27,7 @@ function tomarNumeroTarjeta() {
   const resultadoIsValid = validator.isValid(tNumeroTarjeta);
   const enmascarar = validator.maskify(tNumeroTarjeta);
   if (resultadoIsValid === true) {
-    alert("Tú tarjeta de crédito "+ enmascarar + " es válida, ¡Gracias " + nombre+  " por apadrinar a uno de nuestros peluditos!")
+    alert("Tú tarjeta de crédito " + enmascarar + " es válida, ¡Gracias " + nombre + " por apadrinar a uno de nuestros peluditos!")
   }
   else {
     alert("Tú tarjeta de crédito " + enmascarar + " no es válida, por favor vuelve a ingresarla")
